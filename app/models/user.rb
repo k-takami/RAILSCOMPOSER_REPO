f3575203bd5a19c287db7ad8a95e13cd0fb046fd
k-takami <belltakami@gmail.com>
2018-09-09 10:11:55 +0900

    ( continued )

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   end
 
   # Include default devise modules. Others available are:
+  # NOTE: ":confirmable" module & DB tables are already installed,
+  # However, seeding default user with confirmation flag is not yet implemented.
+  # so you have to manually turns this :confirmable symbol ON
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
